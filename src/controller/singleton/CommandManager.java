f1cf7fdd9fe2504bfcf0712a7f451a320c4c7098
Noureddine <nureddin.aida.1@ens.etsmtl.ca>
2021-11-20 18:19:11 -0500

    CommandManager

--- src/controller/singleton/CommandManager.java
+++ src/controller/singleton/CommandManager.java
@@ -1,4 +1,27 @@
 package controller.singleton;
+import java.util.ArrayList;
 
 public class CommandManager {
+
+        private final static CommandManager manager = new CommandManager();
+        private final LinkedList history = new LinkedList<PerspectiveCommand>();
+        private final ArrayList redoCommands = new ArrayList<PerspectiveCommand>();
+        private final ArrayList undoCommands = new ArrayList<PerspectiveCommand>();
+
+        // Constructeur du singleton
+        private CommandManager() {};
+
+        public static CommandManager getInstance(){
+            return manager;
+        };
+
+        public void dispose(){} // TODO
+
+        public void addUndo(PerspectiveCommand command){
+        undoCommands.add(0, command);
+        }
+
+        public void addRedo(PerspectiveCommand command){
+        redoCommands.add(0, command);
+        }
 }
